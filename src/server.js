92000b0d6ce60ef2787cd925d5f61355d5402b86
oleh27ua <oleh.work27@gmail.com>
2024-10-15 19:34:36 +0300

    Prepared for Vercel

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 });
 
 // Set the views directory
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, './views'));
 
 // Use routes
 app.use('/', routes);
